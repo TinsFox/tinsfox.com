c2a23d4b9d7589a316592574d5e3d68c53dd403c
TinsFox <fox@tinsfox.com>
2023-12-21 21:06:09 +0800

    feat: post

---
+++ pages/api/components.ts
@@ -0,0 +1,13 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  console.log("handler", req.headers);
+  if (req.method !== "GET") {
+    return res.status(405).end();
+  }
+
+  return res.status(200).json("components");
+}
